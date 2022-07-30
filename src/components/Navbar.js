4f79536bb398f6fc8fbb7398525443d549a2b115
Suman Saurabh Das <dsumansaurabh@gmail.com>
2022-07-30 13:22:21 +0530

    Initialised project directory, created folder structure, added tailwind css, added fontawesome and iconscout for logos

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-blue-800">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
