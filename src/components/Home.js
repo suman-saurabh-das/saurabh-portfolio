44578f2f3f15e1bdbad3d8778174ca7954e0f1f6
Suman Saurabh Das <dsumansaurabh@gmail.com>
2022-08-04 21:26:37 +0530

    Added comments to Navbar, Home & Edge components.

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,18 +6,25 @@ export default function Home() {
     <main>
       <section className='dark:bg-blue_10 dark:text-white duration-500 px-4 sm:px-7 md:px-14 lg:px-28 py-4 md:py-8 xl:py-20'>
 
+        {/* Home container */}
         <div className='grid col-span-12 sm:grid-cols-12 items-center'>
           <div className='col-span-7'>
-            {/* content */}
+            
+            {/* Home Content */}
             <p className='font-medium xl:text-lg text-blue_30 dark:text-blue_90 py-2 md:py-3'>Hi my name is,</p>
-            <h1 className='font-bold text-3xl sm:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl text-blue_10 dark:text-blue_50 py-1'>Suman Saurabh Das</h1>
-            <h1 className='font-bold text-xl sm:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl text-blue_10 dark:text-blue_90 py-1'>&lt;I build things for the web/&gt;</h1>
+            <h1 className='font-bold text-3xl sm:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl text-blue_10 dark:text-blue_50 py-1 lg:py-2'>Suman Saurabh Das</h1>
+            <h1 className='font-bold text-xl sm:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl text-blue_10 dark:text-blue_90 py-1 lg:py-2'>&lt;I build things for the web/&gt;</h1>
             <p className='font-medium xl:text-lg text-blue_30 dark:text-blue_90 py-5 w-full sm:w-4/5 lg:w-2/3'>I am a software engineering specializing in building (and occasionally designing) awesome digital experience. I design and code beautifully simple things, and I love what I do. Currently I am working in <span className='text-blue_10 dark:text-blue_50'>Accenture</span>.</p>
+            
+            {/* Resume Download button */}
+            <button type='button' className='py-4 md:py-6 block'>
+              <a target='_blank' rel='noreferrer' href="https://drive.google.com/file/d/1yqYOgTB8W5QAN8B5FG0SwEs5fzVhYErY/view?usp=sharing" className='outline-btn'><i className="uil uil-file-download-alt"></i> resume()</a>
+            </button>
           </div>
 
+          {/* Home Gif */}
           <div className='col-span-12 sm:col-span-5'>
-            {/* coding gif */}
-            <img src={Coding} alt="" />
+            <img src={Coding} alt="Person Coding gif" />
           </div>
         </div>
       </section>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,25 +4,31 @@ export default function Navbar({ darkTheme, setDarkTheme }) {
 
   return (
     <header className='bg-blue_90 dark:bg-blue_10 dark:text-white duration-500'>
+      
+      {/* Navbar Container */}
       <nav className='flex items-center justify-between px-4 sm:px-7 md:px-14 lg:px-8 py-7 md:py-0 lg:py-3 w-full'>
+
+        {/* Navbar logo (for light & dark mode) */}
         <div>
-          {/* logo */}
           <a href="/">
-            <img className='w-8 sm:w-10 lg:w-12 h-8 sm:h-10 lg:h-12' src={darkTheme ? 'https://drive.google.com/uc?export=view&id=1chxlRVlZc3jZ0nc1Ac86xhh--qqxp3fv' : 'https://drive.google.com/uc?export=view&id=1qnwSfFJ2L6i1QnAUdOSiHfqH6vPq3AP0'} alt="Logo" />
+            <img className='w-8 sm:w-10 lg:w-12 h-8 sm:h-10 lg:h-12'
+              src={darkTheme ? 'https://drive.google.com/uc?export=view&id=1chxlRVlZc3jZ0nc1Ac86xhh--qqxp3fv' : 'https://drive.google.com/uc?export=view&id=1qnwSfFJ2L6i1QnAUdOSiHfqH6vPq3AP0'} alt="Logo" />
           </a>
         </div>
 
+        {/* Navigation Menu */}
         <div className='flex items-center justify-between'>
-          {/* navigation toggle button */}
 
+          {/* Hamburger icon toggle checkbox */}
           <input type="checkbox" id='toggle-side-menu' className='absolute h-7 w-7 cursor-pointer opacity-0 md:hidden z-50 peer' />
-          {/* hamburger icon toggle checkbox */}
 
+          {/* Hamburger icon */}
           <div className='z-40'><i className="uil uil-bars text-2xl md:hidden z-40"></i></div>
-          {/* hamburger icon */}
 
+          {/* Navigation Menu Container */}
           <ul className='fixed top-0 -right-2/3 peer-checked:right-0 duration-500 pt-20 md:pt-0 pb-32 md:pb-0 bg-blue_90 w-2/3 md:w-full h-full dark:bg-blue_10 md:static font-semibold flex flex-col md:flex-row items-center justify-around md:justify-center md:text-sm lg:text-base z-30'>
-            {/* menu */}
+
+            {/* Navigation Menu Links */}
             <li className='md:px-2 md:py-3 lg:px-4 lg:py-8'>
               <a className='hover:text-blue_30 dark:hover:text-blue_60' href="/">&lt;About Me/&gt;</a>
             </li>
@@ -35,9 +41,8 @@ export default function Navbar({ darkTheme, setDarkTheme }) {
             <li className='md:px-2 md:py-3 lg:px-4 lg:py-8'>
               <a className='hover:text-blue_30 dark:hover:text-blue_60' href="/">&lt;Contact/&gt;</a>
             </li>
-            <button type='button' className='md:px-2 md:py-8 block'>
-              <a target='_blank' rel='noreferrer' href="https://drive.google.com/file/d/1yqYOgTB8W5QAN8B5FG0SwEs5fzVhYErY/view?usp=sharing" className='outline-btn'><i className="uil uil-file-download-alt"></i> resume()</a>
-            </button>
+
+            {/* Light/Dark mode button */}
             <button type='button' onClick={() => setDarkTheme(!darkTheme)} className='md:px-1 lg:px-2 md:py-8 block'>
               {darkTheme ? <span className="uil uil-brightness outline-btn"> light()</span> : <span className="uil uil-moon outline-btn"> dark()</span>}
             </button>
